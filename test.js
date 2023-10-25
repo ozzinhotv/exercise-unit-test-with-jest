9940cc0556af3966ec8d1e3d9bd15e37aa542f42
ozzinhotv <142857305+ozzinhotv@users.noreply.github.com>
2023-10-25 20:20:47 +0000

    commit

--- test.js
+++ test.js
@@ -22,5 +22,20 @@ test("One euro should be 1.07 dollars", function() {
 
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
-})
+});
+
+test("One dolar should be 146.26 yen", function(){
+    const { fromDollarToYen } = require('./app.js');
+    const yen = fromDollarToYen(1)
+    const expected = (1 * 156.5)/1.07;
+    expect(fromDollarToYen(1)).toBe(146.26168224299064)
+});
+
+test("One yen should be 0.00555911 yen", function(){
+    const { fromYenToPound } = require('./app.js');
+    const yen = fromYenToPound(100)
+    const expected = (100 * 0.87)/156.5
+    expect(fromYenToPound(100)).toBe(0.5559105431309904)
+});
+
 
